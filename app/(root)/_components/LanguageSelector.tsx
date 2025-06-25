6c966919b656f7a3cb2485892536f8d46b4f8097
Ajay Patel <patel.ajay745@gmail.com>
2025-06-24 21:43:48 -0400

    added navbar

---
+++ app/(root)/_components/LanguageSelector.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  hasAccess: boolean;
+}
+
+const LanguageSelector: React.FC<Props> = ({ hasAccess }) => {
+  return <div>LanguageSelector</div>;
+};
+
+export default LanguageSelector;
--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const syncUser = mutation({
     args: {
@@ -16,5 +16,20 @@ export const syncUser = mutation({
             name: args.name,
             isPro: false
         })
+    }
+})
+
+export const getuser = query({
+    args: {
+        userId: v.string()
+    },
+    handler: async (ctx, args) => {
+        if (!args.userId) return null
+
+        const user = await ctx.db.query("users").withIndex("by_user_id").filter((q) => q.eq(q.field("userId"), args.userId)).first()
+
+        if (!user) return null
+
+        return user
     }
 })
