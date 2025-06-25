9b9c5273498568783044295c1942815bd5e0b051
Ajay Patel <patel.ajay745@gmail.com>
2025-06-24 20:29:07 -0400

    first commit

---
+++ app/(root)/_components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = async () => {
+  return <div>Header</div>;
+};
+
+export default Header;
---
+++ components/providers/ConverxClientProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const ConverxClientProvider: React.FC<Props> = ({ children }) => {
+  return (
+    // <ClerkProvider
+    //   publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    // >
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+    // </ClerkProvider>
+  );
+};
+
+export default ConverxClientProvider;
